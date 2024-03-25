19086d7a8d8b4635d70a05f6c5179c6998bd844c
mariaplessia <90716880+mariaplessia@users.noreply.github.com>
2024-03-25 10:26:47 -0400

    Update and rename ps3-1.py to image_editing.py

--- image_editing.py
+++ image_editing.py
@@ -6,7 +6,7 @@ import argparse
 
 def main(args):
     
-    dir = "ps3-images/"
+    dir = "unedited-images/"
     blurry_image = cv2.imread(dir+args.input)
 
 
@@ -19,7 +19,7 @@ def main(args):
 
     # Smoothing & Sharpening Filters for image improvement
 
-    if args.input == 'rainbow.png':
+    if args.input == 'img1.png':
         kernel = np.array([[-1,-1,-1],
                             [-1,9,-1],
                             [-1,-1,-1]])
@@ -30,7 +30,7 @@ def main(args):
         improved = cv2.medianBlur(sharp, 3)
         improved = cv2.bilateralFilter(improved, 15, 85, 85)
 
-    if args.input == 'golf.png':
+    if args.input == 'img2.png':
         kernel = np.array([[-1,-1,-1],
                             [-1,9,-1],
                             [-1,-1,-1]])
@@ -39,7 +39,7 @@ def main(args):
         sharp = cv2.filter2D(smooth, -1, kernel)
         improved = cv2.medianBlur(sharp, 3)
 
-    if args.input == 'pcb.png':
+    if args.input == 'img3.png':
         kernel = np.array([[-1,-1,-1],
                             [-1,9,-1],
                             [-1,-1,-1]])
@@ -48,7 +48,7 @@ def main(args):
         sharp = cv2.filter2D(smooth, -1, kernel)
         improved = cv2.bilateralFilter(sharp, 5, 65, 65)
 
-    if args.input == 'pots.png':
+    if args.input == 'img4.png':
         kernel = np.array([[-1,-1,-1],
                             [-1,9,-1],
                             [-1,-1,-1]])
@@ -56,15 +56,15 @@ def main(args):
         sharp = cv2.filter2D(blurry_image, -1, kernel)
         improved = cv2.bilateralFilter(sharp, 5, 45, 45)
         
-    # Save and display image
-    cv2.imwrite(os.path.splitext(args.input)[0]+"-improved.png", improved)
+    # Save and display edited image
+    cv2.imwrite(os.path.splitext(args.input)[0]+"-edited.png", improved)
     cv2.imshow("Sharp ", improved)
     cv2.waitKey(5000)
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Image improvement via area-to-pixel filters')
-    parser.add_argument('-i', '--input', help='Path to input image.', default='pcb.png')
+    parser.add_argument('-i', default='img1.png')
     args = parser.parse_args()
 
     main(args)
